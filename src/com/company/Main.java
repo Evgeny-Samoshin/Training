a05afeaeea4c34b7818c8539cf36b92e7d751241
Evgeny-Samoshin <samevgeny@gmail.com>
2019-03-25 20:33:36 +0400

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,28 @@ import com.company.Entities.Order;
 import com.company.Entities.Product;
 import com.company.Entities.Seller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	Product meat = new Product("Meat", 350, 2);
-    Customer customer = new Customer("Ivan", "Saratov","+7987");
-    Seller seller = new Seller("Petr","Saratov", "+987");
-    Order order = new Order(meat, customer,seller, 1);
-        System.out.println(order);
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//	Product meat = new Product("Meat", 350, 2);
+//    Customer customer = new Customer("Ivan", "Saratov","+7987");
+//    Seller seller = new Seller("Petr","Saratov", "+987");
+//    Order order = new Order(meat, customer,seller, 1);
+//        System.out.println(order);
+
+
+        Class.forName("org.sqlite.JDBC");
+
+        Connection connection = getConnection("jdbc:sqlite:myDb.db");
+        System.out.println("Connected");
+
+
 
 
     }
